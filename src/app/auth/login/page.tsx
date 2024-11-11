aa1d05c71aa27ee98e7e72611d5c5e76a3d12bda
vannrevan <revanhanafi941@gmail.com>
2024-11-11 22:26:02 +0700

    initial commit

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,109 @@
+"use client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  password: z.string().min(2, {
+    message: "Password must be at least 2 characters.",
+  }),
+});
+
+const Login = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+  return (
+    <div className="min-h-dvh flex justify-center items-center">
+      <Card className="w-[32rem]">
+        <CardHeader>
+          <CardTitle>Halaman Login</CardTitle>
+          <CardDescription>Login sebelum melanjutkan...</CardDescription>
+        </CardHeader>
+        <Separator />
+        <CardContent className="p-6">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="username"
+                        {...field}
+                        value={field.value || ""}
+                        onChange={(e) => field.onChange(e)}
+                      />
+                    </FormControl>
+                    <FormDescription>Masukin username kamu.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        value={field.value || ""}
+                        onChange={(e) => field.onChange(e)}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Masukin password kamu disini juga yaa.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+        </CardContent>
+        <Separator />
+        <CardFooter className="p-6 flex justify-end">
+          <Button type="submit">Login</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default Login;
